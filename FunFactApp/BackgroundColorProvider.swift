333ec9d00ab3513325153e20ebad49935561c3cb
Ismael Zeitoun Soumahoro <Izzysoum@Bens-iMac.home>
2018-07-20 19:38:38 -0400

    test

---
+++ FunFactApp/BackgroundColorProvider.swift
@@ -0,0 +1,9 @@
+//
+//  BackgroundColorProvider.swift
+//  FunFactApp
+//
+//  Created by Ismael Zeitoun Soumahoro on 8/11/17.
+//  Copyright © 2017 Ismael Zeitoun Soumahoro. All rights reserved.
+//
+
+import Foundation
---
+++ FunFactApp/FactProvider.swift
@@ -0,0 +1,9 @@
+//
+//  FactProvider.swift
+//  FunFactApp
+//
+//  Created by Ismael Zeitoun Soumahoro on 8/9/17.
+//  Copyright © 2017 Ismael Zeitoun Soumahoro. All rights reserved.
+//
+
+import Foundation
